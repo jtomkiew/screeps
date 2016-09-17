14988847603ac8912b18a56fc80b8656b95e296a
jtomkiew <spratfear@gmail.com>
2016-09-17 10:09:41 +0200

    towers lambda

--- src/main.js
+++ src/main.js
@@ -27,28 +27,25 @@ module.exports.loop = function () {
     var spawn = Game.spawns['Spawn1']
 
     // TOWERS
-    var towers = spawn.room.find(FIND_STRUCTURES, {
+    var towers = spawn.room.find(FIND_MY_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_TOWER &&
                                 structure.energy > 0);
                     }
     });
-    for(var tower in towers){
-        if(tower) {
-            tower = Game.getObjectById(tower.id)
-            var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-            if(closestHostile) {
-                tower.attack(closestHostile);
-            }
-
-            var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (structure) => structure.hits < structure.hitsMax
-            });
-            if(closestDamagedStructure) {
-                tower.repair(closestDamagedStructure);
-            }
+    towers.forEach(tower => {
+        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if(closestHostile) {
+            tower.attack(closestHostile);
         }
-    }
+
+        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if(closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+    });
 
     // ENERGY
     var energyCurrent = spawn.room.energyAvailable;
