f8b17cde672681831752ad0fc880ea79e0d80fbe
jtomkiew <spratfear@gmail.com>
2016-09-17 10:06:09 +0200

    towers

--- src/main.js
+++ src/main.js
@@ -26,28 +26,29 @@ module.exports.loop = function () {
 
     var spawn = Game.spawns['Spawn1']
 
-    // // TOWERS
-    // var towers = spawn.room.find(FIND_STRUCTURES, {
-    //                 filter: (structure) => {
-    //                     return (structure.structureType == STRUCTURE_TOWER &&
-    //                             structure.energy > 0);
-    //                 }
-    // });
-    // for(var tower in towers){
-    //     if(tower) {
-    //         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-    //         if(closestHostile) {
-    //             tower.attack(closestHostile);
-    //         }
+    // TOWERS
+    var towers = spawn.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_TOWER &&
+                                structure.energy > 0);
+                    }
+    });
+    for(var tower in towers){
+        if(tower) {
+            tower = Game.getObjectById(tower.id)
+            var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            if(closestHostile) {
+                tower.attack(closestHostile);
+            }
 
-    //         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-    //             filter: (structure) => structure.hits < structure.hitsMax
-    //         });
-    //         if(closestDamagedStructure) {
-    //             tower.repair(closestDamagedStructure);
-    //         }
-    //     }
-    // }
+            var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (structure) => structure.hits < structure.hitsMax
+            });
+            if(closestDamagedStructure) {
+                tower.repair(closestDamagedStructure);
+            }
+        }
+    }
 
     // ENERGY
     var energyCurrent = spawn.room.energyAvailable;
